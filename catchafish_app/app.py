d59870ad82d58880833d4f74ac8567352900ddbe
Paul Emmanuel Chouc <pechouc@gmail.com>
2020-07-01 23:22:24 +0200

    Some work on the frontend

--- catchafish_app/app.py
+++ catchafish_app/app.py
@@ -1,40 +1,137 @@
 import streamlit as st
-from skimage.io import imread,imsave
+from skimage.io import imread, imsave
 from skimage.transform import resize
 import numpy as np
 import wikipedia
 import os
 
-from catchafish_app.utils import predict
-from catchafish_app.utils import PROJECT_ID, BUCKET_NAME, BUCKET_MODEL_NAME, MODEL_VERSION
+from catchafish_app.utils import predict, get_additional_images, add_image_to_dataset
+from catchafish_app.utils import PROJECT_ID, BUCKET_NAME, BUCKET_MODEL_NAME, MODEL_VERSION, NAMES_MAPPING
 
-credentials = os.environ["GOOGLE_CREDENTIALS"] #dictionary
-json_file = open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], 'w')
-json_file.write(credentials)
-json_file.close()
+#credentials = os.environ["GOOGLE_CREDENTIALS"] #dictionary
+#json_file = open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], 'w')
+#json_file.write(credentials)
+#json_file.close()
 
-# Display title
-st.title('Catch a fish !')
+mode = st.sidebar.selectbox("👇 Select your profile", ["Curious", "Expert"])
 
-# Upload image to predict
-uploaded_image = st.file_uploader("Choose image to predict")
-if uploaded_image is not None:
-    image = imread(uploaded_image)
-    st.image(image)
-    image = resize(image, (128, 128))
-    image = np.expand_dims(image, axis = 0)
+if mode == "Curious":
 
-# Predict fish
-if uploaded_image:
-    instances = image.tolist()
+    # Display title
+    st.title('Catch a fish!')
 
-    result = predict(
-        project=PROJECT_ID,
-        model=BUCKET_MODEL_NAME,
-        version=MODEL_VERSION,
-        instances=instances
-        )
+    # Upload image to predict
+    uploaded_image = st.file_uploader("Upload an image and get the species of your fish 🐠")
 
-    st.markdown(f'{result}')
-    st.markdown('This fish is a [gold fish](https://fr.wikipedia.org/wiki/Poisson-clown).')
-    st.markdown(wikipedia.summary("goldfish",sentences=2))
+    if uploaded_image is not None:
+        # Storing the image into a NumPy array and plotting it
+        image = imread(uploaded_image)
+        st.image(image, use_column_width = True)
+
+        # Predict fish
+        image_preprocessed = resize(image, (128, 128))
+        image_preprocessed = np.expand_dims(image_preprocessed, axis = 0)
+
+        instances = image_preprocessed.tolist()
+
+        predicted_class = predict(
+            project=PROJECT_ID,
+            model=BUCKET_MODEL_NAME,
+            version=MODEL_VERSION,
+            instances=instances
+            )
+
+        result = NAMES_MAPPING[predicted_class][1]
+
+        # Display model prediction
+        if result.startswith(('A', 'E', 'I', 'O', 'U')):
+            st.markdown(f'## 👉 &nbsp;This fish is an **{result}**')
+        else:
+            st.markdown(f'## 👉 &nbsp;This fish is a **{result}**')
+
+        # Get information from wikipedia and display it
+        scrapped = wikipedia.summary(result, sentences = 2)
+
+        if '\n' in scrapped:
+            scrapped = scrapped.replace('\n', '').replace('.', '. ')
+
+        if '==' in scrapped:
+            scrapped = wikipedia.summary(result, sentences = 1)
+
+        st.markdown(scrapped)
+
+        # Adding a separator
+        st.markdown('---')
+
+        # Displaying other images from the same class
+        st.markdown('## 📷 &nbsp;Other images posted on the app for this species')
+
+        img_0, img_1, img_2 = get_additional_images(predicted_class)
+        st.image([img_0, img_1, img_2], width = 210)
+        os.system('rm img_0.jpg img_1.jpg img_2.jpg')
+
+        # Adding a separator
+        st.markdown('---')
+
+
+elif mode == "Expert":
+
+    # Display title
+    st.title('Catch a fish!')
+
+    # Upload image to predict
+    uploaded_image = st.file_uploader("Upload an image and get the species of your fish 🐠")
+
+    if uploaded_image is not None:
+        # Storing the image into a NumPy array and plotting it
+        image = imread(uploaded_image)
+        st.image(image, use_column_width = True)
+
+        # Predict fish
+        image_preprocessed = resize(image, (128, 128))
+        image_preprocessed = np.expand_dims(image_preprocessed, axis = 0)
+
+        instances = image_preprocessed.tolist()
+
+        predicted_class = predict(
+            project=PROJECT_ID,
+            model=BUCKET_MODEL_NAME,
+            version=MODEL_VERSION,
+            instances=instances
+            )
+
+        result = NAMES_MAPPING[predicted_class][1]
+
+        # Display model prediction
+        if result.startswith(('A', 'E', 'I', 'O', 'U')):
+            st.markdown(f'## 👉 This fish is an **{result}**')
+        else:
+            st.markdown(f'## 👉 This fish is a **{result}**')
+
+        # Get information from wikipedia and display it
+        scrapped = wikipedia.summary(result, sentences = 2)
+
+        if '\n' in scrapped:
+            scrapped = scrapped.replace('\n', '').replace('.', '. ')
+
+        if '==' in scrapped:
+            scrapped = wikipedia.summary(result, sentences = 1)
+
+        st.markdown(scrapped)
+
+        # Adding a separator
+        st.markdown('---')
+
+        # Giving the possibility to annotate the image
+        st.markdown('## ⁉️ You feel like the identification is wrong?')
+        st.markdown('If you are positive the algorithm is mistaken on this call, please provide us with the expected species and this additional data point will be used for further model training!')
+        species = ['Mysterius fishii?'] + [elt[1] for elt in NAMES_MAPPING.values()]
+        correct_pred = st.selectbox('One small step for man, one giant leap for the model 🔭', species)
+
+        # Adding a separator
+        st.markdown('---')
+
+        # Feeding the new data point to the model
+        if correct_pred != 'Mysterius fishii?':
+            message = add_image_to_dataset(image, correct_pred)
+            st.markdown(f'*{message}*')
--- catchafish_app/utils.py
+++ catchafish_app/utils.py
@@ -1,8 +1,12 @@
 import json
 import numpy as np
 import pandas as pd
+from skimage.io import imread, imsave
+import os
+
 from google.cloud import storage
 import googleapiclient.discovery
+from google.oauth2 import service_account
 
 PROJECT_ID='wagon-project-catchafish'
 BUCKET_NAME='catchafish_gcp'
@@ -24,6 +28,17 @@ NAMES_MAPPING = {
     10: ("fish_17", "Carassius auratus")
     }
 
+def get_credentials():
+    credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+
+    if '.json' in credentials_raw:
+        credentials_raw = open(credentials_raw).read()
+
+    creds_json = json.loads(credentials_raw)
+    creds_gcp = service_account.Credentials.from_service_account_info(creds_json)
+
+    return creds_gcp
+
 def predict(project, model, version, instances):
     '''Call model for prediction'''
 
@@ -41,6 +56,34 @@ def predict(project, model, version, instances):
 
     results = response['predictions']
 
-    predicted_class = np.argmax(results[0]['dense_1'])
+    return np.argmax(results[0]['dense_1'])
 
-    return NAMES_MAPPING[predicted_class][1]
+def get_additional_images(predicted_class):
+    creds = get_credentials()
+    client = storage.Client(credentials = creds, project = PROJECT_ID)
+    client = client.bucket(BUCKET_NAME)
+
+    folder_name = NAMES_MAPPING[predicted_class][0]
+    storage_location = f'data/{folder_name}/'
+
+    storage_location_0 = storage_location + '0.jpg'
+    blob_0 = client.blob(storage_location_0)
+    blob_0.download_to_filename('img_0.jpg')
+    img_0 = imread('img_0.jpg')
+
+    storage_location_1 = storage_location + '1.jpg'
+    blob_1 = client.blob(storage_location_1)
+    blob_1.download_to_filename('img_1.jpg')
+    img_1 = imread('img_1.jpg')
+
+    storage_location_2 = storage_location + '2.jpg'
+    blob_2 = client.blob(storage_location_2)
+    blob_2.download_to_filename('img_2.jpg')
+    img_2 = imread('img_2.jpg')
+
+    return img_0, img_1, img_2
+
+def add_image_to_dataset(image, species):
+    folder_name = [elt[0] for elt in NAMES_MAPPING.values() if elt[1] == species][0]
+    return f'Uploaded successfully to /data/{folder_name} - Dummy message'
+
